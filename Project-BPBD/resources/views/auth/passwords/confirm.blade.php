051715dba7ce711b3b0f809acc06fac8c38fab71
Aldo Faiz Winarno <71934691+aldofaiz@users.noreply.github.com>
2021-08-18 12:36:04 +0700

    lupa password

--- Project-BPBD/resources/views/auth/passwords/confirm.blade.php
+++ Project-BPBD/resources/views/auth/passwords/confirm.blade.php
@@ -1,49 +1,54 @@
-@extends('layouts.app')
+@extends('auth\layout\main')
+
+@section('style')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}"></link>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+<body>
+    <x-header />
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
-
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <!-- form login  -->
+    <div class="d-flex flex-column align-items-center login-tengah container">
+        <div class='form-login' id="form-login">
+        <div class="namelogin"><b>{{ __('Confirm Password') }}</b></div>
+        <div class="d-flex flex-column justify-content-start">
+            {{ __('Please confirm your password before continuing.') }}
+            <br><br>
+            <form method="POST" action="{{ route('password.confirm') }}">
+                @csrf
+                
+                <label for="password" class="form-label form-login-text "> Password</label>
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                
+                <div class="div-button-login">
+                    <button type="submit" class="button-login"><b>{{ __('Confirm Password') }}</b></button>
+                    <br><br>
+                    @if (Route::has('password.request'))
+                    <div class="div-lupapassword">
+                    <a class="a-lupapassword" href="{{ route('password.request') }}">
+                        Lupa Password?
+                    </a>
+                    </div>
+                    @endif  
+                </div>             
+            </form>
             </div>
         </div>
-    </div>
-</div>
+    </div> 
+    <!-- end form login -->
+
+    <x-footer />
+
+    <!-- script  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
 @endsection
--- Project-BPBD/resources/views/auth/verify.blade.php
+++ Project-BPBD/resources/views/auth/verify.blade.php
@@ -1,28 +1,41 @@
-@extends('layouts.app')
+@extends('auth\layout\main')
+
+@section('style')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}"></link>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<body>
+    <x-header />
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+    <!-- form login  -->
+    <div class="d-flex flex-column align-items-center login-tengah container">
+        <div class='form-login' id="form-login">
+        <div class="namelogin"><b>{{ __('Verify Your Email Address') }}</b></div>
+        <div class="d-flex flex-column justify-content-start">
+            @if (session('resent'))
+                <div class="alert alert-success" role="alert">
+                    {{ __('A fresh verification link has been sent to your email address.') }}
                 </div>
+            @endif
+
+            {{ __('Before proceeding, please check your email for a verification link.') }}
+            {{ __('If you did not receive the email') }},
+            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                @csrf
+                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+            </form>
             </div>
         </div>
-    </div>
-</div>
+    </div> 
+    <!-- end form login -->
+
+    <x-footer />
+
+    <!-- script  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
 @endsection
